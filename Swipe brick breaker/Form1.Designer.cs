fe3784cc820c677874abb0aaba9fbbbd7b2bc8bd
Abbosbek Niyozqulov <abbos6882@gmail.com>
2020-03-06 11:14:58 +0500

    Добавьте файлы проекта.

---
+++ Swipe brick breaker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Swipe_brick_breaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblBall = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.Startpage = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Startpage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(424, 611);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 23);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // lblBall
+            // 
+            this.lblBall.BackColor = System.Drawing.Color.White;
+            this.lblBall.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBall.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblBall.Location = new System.Drawing.Point(12, 648);
+            this.lblBall.Name = "lblBall";
+            this.lblBall.Size = new System.Drawing.Size(184, 32);
+            this.lblBall.TabIndex = 5;
+            this.lblBall.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Yellow;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Algerian", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(479, 229);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(153, 55);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.Yellow;
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnQuit.Font = new System.Drawing.Font("Algerian", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.Location = new System.Drawing.Point(479, 315);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(153, 55);
+            this.btnQuit.TabIndex = 7;
+            this.btnQuit.Text = "QUIT";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // Startpage
+            // 
+            this.Startpage.BackColor = System.Drawing.Color.Green;
+            this.Startpage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Startpage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Startpage.Location = new System.Drawing.Point(0, 0);
+            this.Startpage.Name = "Startpage";
+            this.Startpage.Size = new System.Drawing.Size(1093, 689);
+            this.Startpage.TabIndex = 3;
+            this.Startpage.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(290, 241);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1093, 689);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1093, 689);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblBall);
+            this.Controls.Add(this.Startpage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BRICK BREAKER";
+            ((System.ComponentModel.ISupportInitialize)(this.Startpage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox Startpage;
+        private System.Windows.Forms.Label lblBall;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ Swipe brick breaker/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swipe_brick_breaker
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        private static int n = 24;
+        private Random rn = new Random(); Timer t = new Timer();
+        private int x, y, i = 0, k = 0, bw, bh, ball = 0, picw=15,pich=15, fastcount=0;
+        private bool xb = true, yb = true;
+        Button[] btn = new Button[n];
+        public Form1()
+        {
+            InitializeComponent();
+            bw = pictureBox1.Width / 10;
+            bh = bw / 2;
+            for (int j = 0; j < n; j++)
+            {
+                if (j % 8 == 0)
+                    k++;
+                btn[j] = new Button();
+                btn[j].SetBounds(j % 8 * bw+bw, k * bh, bw, bh);
+                btn[j].Text = "";
+                btn[j].BackColor = Color.Blue;
+                btn[j].Enabled = false;
+                this.Controls.Add(btn[j]);
+                btn[j].BringToFront();
+            }
+            Startpage.BringToFront();
+            btnStart.BringToFront();
+            btnQuit.BringToFront();
+            pictureBox2.Location = new Point(r.Next(0, pictureBox1.Width - 30), r.Next(100, 300));
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (y < pictureBox1.Height)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (btn[j].Visible)
+                    {
+                        if (x == btn[j].Location.X + bw && y <= btn[j].Location.Y + bh && y + pich >= btn[j].Location.Y - pich)
+                        {
+                            xb = true;
+                            btn[j].Visible = false;
+                            ball++;
+                        }
+                        if (x + picw == btn[j].Location.X && y <= btn[j].Location.Y + bh && y + pich >= btn[j].Location.Y)
+                        {
+                            xb = false;
+                            btn[j].Visible = false;
+                            ball++;
+                        }
+                        if (y == btn[j].Location.Y + bh && x <= btn[j].Location.X + bw && x >= btn[j].Location.X - picw)
+                        {
+                            yb = true;
+                            btn[j].Visible = false;
+                            ball++;
+                        }
+                        if (y + pich == btn[j].Location.Y &&x<= btn[j].Location.X + bw && x >= btn[j].Location.X - picw)
+                        {
+                            yb = false;
+                            btn[j].Visible = false;
+                            ball++;
+                        }
+                        lblBall.Text = Convert.ToString(ball);
+                    }
+                    else
+                    {
+                        if (ball == n)
+                        {
+                            ball = 0;
+                            while (i > 0)
+                            {
+                                t.Tick -= new EventHandler(timer1_Tick);
+                                i--;
+                            }
+                            MessageBox.Show("YOU WIN");
+                            Startpage.Visible = true;
+                            Startpage.BringToFront();
+                            btnStart.Visible = true;
+                            btnStart.BringToFront();
+                            btnQuit.Visible = true;
+                            btnQuit.BringToFront();
+                        }
+                    }
+                }
+                pictureBox2.Location = new Point(x, y);
+                if (x == 0)
+                {
+                    xb = true;
+                }
+                if (x == pictureBox1.Width - pictureBox2.Width)
+                {
+                    xb = false;
+                }
+                if (y == 0)
+                {
+                    yb = true;
+                }
+                if (y + pictureBox2.Height == button1.Location.Y && x + pictureBox2.Width / 2 <= button1.Location.X + button1.Width && x + pictureBox2.Width / 2 >= button1.Location.X)
+                {
+                    yb = false;
+                    fastcount++;
+                    if (fastcount == 2)
+                    {
+                        fastcount = 0;
+                        i++;
+                        t.Tick += new EventHandler(timer1_Tick);
+                        t.Start();
+                    }
+                }
+                if (y + pictureBox2.Height == button1.Location.Y && x + pictureBox2.Width / 2 <= button1.Location.X && x + pictureBox2.Width / 2 >= button1.Location.X - pictureBox2.Width/2)
+                {
+                    yb = false;
+                    xb = false;
+                    fastcount++;
+                    if (fastcount == 2)
+                    {
+                        fastcount = 0;
+                        i++;
+                        t.Tick += new EventHandler(timer1_Tick);
+                        t.Start();
+                    }
+                }
+                if (y + pictureBox2.Height == button1.Location.Y && x + pictureBox2.Width / 2 <= button1.Location.X + button1.Width + pictureBox2.Width/2 && x + pictureBox2.Width / 2 >= button1.Location.X + button1.Width)
+                {
+                    yb = false;
+                    xb = true;
+                    fastcount++;
+                    if (fastcount == 2)
+                    {
+                        fastcount = 0;
+                        i++;
+                        t.Tick += new EventHandler(timer1_Tick);
+                        t.Start();
+                    }
+                }
+                x = xb ? x + 1 : x - 1;
+                y = yb ? y + 1 : y - 1;
+            }
+            else
+            {
+                y = 293;
+                while (i > 0)
+                {
+                    t.Tick -= new EventHandler(timer1_Tick);
+                    i--;
+                }
+                MessageBox.Show("GAME OVER");
+                Startpage.Visible = true;
+                Startpage.BringToFront();
+                btnStart.BringToFront();
+                btnStart.Visible = true;
+                btnQuit.Visible = true;
+                btnQuit.BringToFront();
+            }
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X < pictureBox1.Width - button1.Width)
+            {
+                button1.Location = new Point(e.X, button1.Location.Y);
+            }
+            else
+            {
+                button1.Location = new Point(pictureBox1.Width - button1.Width, button1.Location.Y);
+            }
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Startpage.Visible = false;
+            btnStart.Visible = false;
+            btnQuit.Visible = false;
+            t.Interval = 1;
+            i++;
+            t.Tick += new EventHandler(timer1_Tick);
+            x = 500;
+            y = 280;
+            ball = 0;
+            t.Start();
+
+            for (int j = 0; j < 16; j++)
+            {
+                btn[j].Visible = true;
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
